d3de537cb2c043fbb9fed213cf610e9cbd8685c0
MarcoMoro10 <marco.morelli15@studio.unibo.it>
2025-03-09 18:10:14 +0100

    testKnightMove

--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -1,6 +1,7 @@
 package e2;
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LogicTest {
@@ -23,6 +24,26 @@ public class LogicTest {
     }
     assertTrue(validKnight, "The horse must be on the correct position");
     assertTrue(validPawn, "The pawn must be on the correct position");
+  }
+  @Test
+  void testKnightValidMove(){
+    int knightRow = -1, knightCol = -1;
+    for (int row = 0; row < SIZE; row++) {
+      for (int col = 0; col < SIZE; col++) {
+        if (logics.hasKnight(row, col)) {
+          knightRow = row;
+          knightCol = col;
+          break;
+        }
+      }
+      if (knightRow != -1) break;
+    }
+    int newRow = knightRow + 2;
+    int newCol = knightCol + 1;
+    if (newRow < SIZE && newCol < SIZE) {
+      assertFalse(logics.hit(newRow, newCol), "The Knight should move without hit the pawn");
+      assertTrue(logics.hasKnight(newRow, newCol), "The Knight should be on the correct position");
+    }
   }
 
 }
